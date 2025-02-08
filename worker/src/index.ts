8d7e22d9b5fdd4fffde24f6457bf0e5b788992be
aj1234530 <youmailmejust123@gmail.com>
2025-02-08 20:36:17 +0530

    minor change

--- worker/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { main } from './userRegistration';
-main();
-
--- worker/src/redisClients.ts
+++
@@ -1,7 +0,0 @@
-import {createClient} from 'redis';
-export const redisClient = createClient();
-async function redisConnect() {
-    await redisClient.connect(); 
-    console.log("redis connected")
-}
-redisConnect();
